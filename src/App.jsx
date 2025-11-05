3f4835dd04ac69baadd9999d3c7e48d03020707c
hafeezrafeeq <hafeezrafeeq031@gmail.com>
2025-11-05 21:15:38 +0500

    Final project build and design completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar'
 import GreenArea from './components/GreenArea'
 import About from './components/About'
 import Eyes from './components/Eyes'
-import Featured from './components/featured'
+import Featured from './components/Featured'
 import Reviews from './components/Reviews';
 import StartProject from './components/StartProject';
 import Footer from './components/Footer';
